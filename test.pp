651280ed856132c9e2479284bb37ec4b951b9b5c
k1574 <k1574@yandex.ru>
2021-07-15 07:32:17 +0500

    More tests.

--- test.pp
+++ test.pp
@@ -1,4 +1,31 @@
+#!/bin/env pp
+# Test
+
+## Test of setting variables:
+
 <{= var value}><{= dash-var dash value}>
-var = "<{$ var}>"
-dash-var = "<{$ dash-var}>"
-dash_var = "<{$ dash_var}>"
+	var = "<{$ var}>"
+	dash-var = "<{$ dash-var}>"
+	dash_var = "<{$ dash_var}>"
+
+## Command executing
+
+`{uname} = <{! uname}>
+`{echo `{echo fuck}} = "<{! echo <{! echo fuck}>}>"
+
+## Preprocessing text with other commands
+
+<{| rc}>
+	echo `{echo This shit works with any command and even rc}
+	date
+<{#|}>
+
+<{| sh}>
+	echo `echo Even with sh`
+	ls
+<{#|}>
+
+<{| sed s/gay/homosexual/g}>
+Now even gays cannot be called gays.
+Isn't it great?
+<{#|}>
